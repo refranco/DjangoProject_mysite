e318b0c1eb6af2815e952df894f2bd0bdcf14abe
refranco <estebanfrancobedoya@gmail.com>
2021-07-12 11:46:37 -0500

    codigo para tarea Classified Ads # 1 funcionando local, no funciona el social login

---
+++ ads/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from .models import Ad
+from .owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+
+class AdListView(OwnerListView):
+    model = Ad
+    # By convention:
+    # template_name = "ads/ad_list.html"
+
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+
+
+class AdCreateView(OwnerCreateView):
+    model = Ad
+    fields = ['title','price', 'text']
+      
+
+class AdUpdateView(OwnerUpdateView):
+    model = Ad
+    fields = ['title','price', 'text']
+
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
+
